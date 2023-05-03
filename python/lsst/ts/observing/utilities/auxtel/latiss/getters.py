f50fa762278c3764ba8896053f0ef311fbc328a6
Tiago Ribeiro <tribeiro@lsst.org>
2023-05-03 14:56:08 -0700

    In ``auxtel/latiss/getters``, remove inline logger and pass an optional logger to ``get_image``.
    If no logger is provided, one is created to log information.

--- python/lsst/ts/observing/utilities/auxtel/latiss/getters.py
+++ python/lsst/ts/observing/utilities/auxtel/latiss/getters.py
@@ -5,19 +5,13 @@ import typing
 
 from lsst.afw.image import ExposureF
 
-STD_TIMEOUT = 10  # seconds
-
-logging.basicConfig()
-# Make matplotlib less chatty
-logging.getLogger("matplotlib").setLevel(logging.WARNING)
-logger = logging.getLogger(__name__)
-
 
 async def get_image(
     data_id: dict[str, int | str],
     best_effort_isr: typing.Any,
     timeout: float,
     loop_time: float = 0.1,
+    log: None | logging.Logger = None,
 ) -> ExposureF:
     """
     Retrieve image from butler repository.
@@ -26,23 +20,28 @@ async def get_image(
 
     Parameters
     ----------
-    data_id: `dict`
+    data_id : `dict`
         A dictionary consisting of the keys and data required to fetch an
         image from the butler.
         e.g data_id = {'day_obs': 20200219, 'seq_num': 2,
                        'detector': 0, "instrument": 'LATISS'}
-    best_effort_isr: `object`
+    best_effort_isr : `object`
         BestEffortISR class instantiated with a butler.
-    loop_time: `float`
-        Time between polling attempts. Defaults to 0.1s
-    timeout: `float`
-        Total time to poll for image before raising an exception
+    loop_time : `float`
+        Time between polling attempts. Defaults to 0.1s.
+    timeout : `float`
+        Total time to poll for image before raising an exception.
+    log : `logging.Logger`, optional
+        An optional logger to use when logging information. If not given,
+        creates one.
 
     Returns
     -------
     exp: `ExposureF`
         Exposure returned from butler query
     """
+
+    logger = log.getChild("get_image") if log is not None else logging.getLogger("get_image")
 
     endtime = time.time() + timeout
     while True:
@@ -55,8 +54,7 @@ async def get_image(
 
         except ValueError:
             logger.exception(
-                f"Could not get new image from butler. Waiting "
-                f"{loop_time} seconds and trying again."
+                f"Could not get new image from butler. Waiting {loop_time} seconds and trying again."
             )
             await asyncio.sleep(loop_time)
 
